922abfdfccc124d065c56a22ad1318cdf3dbedaf
nathandaniels <nathandaniels@mac.com>
2025-02-15 14:57:05 +1030

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         ),
     ], 
     dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.19.0"),
+        .package(url: "https://github.com/apple/swift-protobuf.git", exact: "1.28.1"),
     ],
     targets: [
         .target(
